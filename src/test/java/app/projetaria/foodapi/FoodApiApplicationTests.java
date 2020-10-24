c38e4754040fd8358e22277986f37e9fd669a0fe
danilotiago <programacao.desenvolvimento@gmail.com>
2020-10-24 12:40:05 -0300

    inicial

---
+++ src/test/java/app/projetaria/foodapi/FoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package app.projetaria.foodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
